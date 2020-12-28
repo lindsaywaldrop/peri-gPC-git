71222c5484781a4cbef282eae24d09b99331b3b9
lindsaywaldrop <waldrop@chapman.edu>
2020-12-28 15:32:27 -0800

    Changing the check runs to include hier data

--- src/r-scripts/data_handling_functions.R
+++ src/r-scripts/data_handling_functions.R
@@ -1,17 +1,28 @@
 # Data handling functions
 
-loadflowdata <- function(track,date){
+# Functions for non-dimensionalizing values
+rmdim.speed <- function(speeds, dia) speeds/dia
+rmdim.pressure <- function(pressure, dia, density) pressure/(density*dia^2)
+rmdim.volflowrate <- function(Q, dia) Q/(dia^2)
+
+# Loading flow data
+loadflowdata <- function(track, date, dia, density){
   data <- read.csv(paste("../results/r-csv-files/", track, 
                          "_results/combined_data_",track,"_165_", date, ".csv", sep = ""), 
                    header = TRUE)
-  data$delta_P <- data$vena_P_avg-data$aorta_P_avg
+  U.names <- subset(colnames(data), grepl({"U"}, colnames(data)))
+  for(y in 1:length(U.names)) data[[U.names[y]]] <- rmdim.speed(data[[U.names[y]]], dia)
+  P.names<- subset(colnames(data), grepl({"P"}, colnames(data)))
+  for(y in 1:length(P.names)) data[[P.names[y]]] <- rmdim.pressure(data[[P.names[y]]], dia, density)
+  data$delta_P <- data$vena_P_avg - data$aorta_P_avg
   data.Q <- read.csv(paste("../results/r-csv-files/", track, 
                            "_results/Qall_165_", date, ".csv", sep = ""), 
                      header = TRUE)
-  data$Q <- data.Q$Qall
+  data$Q <- rmdim.volflowrate(data.Q$Qall, dia)
   return(data)
 }
 
+# Loading cost of transport and work data
 loadcostdata <- function(track, date){
   data <- read.csv(paste("../results/matlab-csv-files/", track, 
                          "_results/cost_of_transport_165_", date, ".csv", sep = ""),
@@ -20,6 +31,7 @@ loadcostdata <- function(track, date){
   return(data)
 }
 
+# Combining flow and COT data
 combinedata <- function(flowdata,costdata) {
   if(is.na(costdata)){ 
     costdata <- data.frame("COT" =rep(NA,dim(flowdata)[1]), 
@@ -27,6 +39,7 @@ combinedata <- function(flowdata,costdata) {
   data <- cbind(flowdata,costdata)
 }
 
+# Combining data from multiple tracks after running above functions
 combinetracks <- function(data, tracklist){
   numtracks <- length(tracklist)
   alldata <- data[[1]]
@@ -41,8 +54,7 @@ combinetracks <- function(data, tracklist){
   return(alldata)
 }
 
-
-
+# Loading and shaping data for plotting tracks
 plot.track <- function(track){
   race.data <- read.table(paste("../data/ibamr-files/", track,
                                 "_files/heart_race_512.vertex", sep = ""),
